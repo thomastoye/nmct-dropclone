68df6f43cfd91b6e4e3df38d9c5a62160fa54d3a
Thomas Toye <toye.thomas@gmail.com>
2014-12-08 22:51:40 +0100

    Add file DB models

---
+++ nmct.ssa.dropbox/Models/FileRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ssa.dropbox.Models
+{
+    public class FileRegistration
+    {
+        public int FileId { get; set; }
+        public string Description { get; set; }
+        public string FileName { get; set; }
+        public DateTime UploadTime { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ nmct.ssa.dropbox/Models/FileUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ssa.dropbox.Models
+{
+    public class FileUser
+    {
+        public int FileId { get; set; }
+        public string UserName { get; set; }
+    }
+}
