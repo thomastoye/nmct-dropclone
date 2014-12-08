950df0929c9d8fabad7d3049e07ef734080ca061
Thomas Toye <toye.thomas@gmail.com>
2014-12-08 19:44:43 +0100

    Initial commit

---
+++ nmct.ssa.dropbox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nmct.ssa.dropbox.MvcApplication" Language="C#" %>
---
+++ nmct.ssa.dropbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nmct.ssa.dropbox.Startup))]
+namespace nmct.ssa.dropbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
