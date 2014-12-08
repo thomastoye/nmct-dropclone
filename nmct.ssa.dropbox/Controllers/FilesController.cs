2b9066da4e31ccab46af4b993a5745335b092614
Thomas Toye <toye.thomas@gmail.com>
2014-12-08 22:41:35 +0100

    Upload functionality

--- nmct.ssa.dropbox/Controllers/FilesController.cs
+++ nmct.ssa.dropbox/Controllers/FilesController.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using nmct.ssa.dropbox.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace nmct.ssa.dropbox.Controllers
 {
+    [Authorize]
     public class FilesController : Controller
     {
         // GET: Files
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            var context = new ApplicationDbContext();
+            ViewBag.Users = context.Users;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file, string description, string[] wie)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/app_data/uploads"), filename);
+                file.SaveAs(path);
+            }
+            return RedirectToAction("Index");
+
         }
     }
 }
