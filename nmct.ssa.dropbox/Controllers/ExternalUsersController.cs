8abb2976b4cb2d342e57a7b50c9294b1968fde98
Thomas Toye <toye.thomas@gmail.com>
2014-12-09 22:56:42 +0100

    Add external user functionality
    Stap 1 van het labo

---
+++ nmct.ssa.dropbox/Controllers/ExternalUsersController.cs
@@ -0,0 +1,52 @@
+﻿using nmct.ssa.dropbox.common;
+using nmct.ssa.dropbox.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nmct.ssa.dropbox.Controllers
+{
+    public class ExternalUsersController : Controller
+    {
+        [HttpGet]
+        [Authorize(Roles="Administrator")]
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+        
+        public ActionResult List(){
+            ViewBag.Models = DAExternalUser.GetExternalUsers();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ExternalUser ext = DAExternalUser.GetById(id);
+            return View(ext);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ExternalUser ext)
+        {
+            DAExternalUser.Update(ext);
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ExternalUser ext)
+        {
+            DAExternalUser.New(ext);
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ nmct.ssa.dropbox/DataAccess/DAExternalUser.cs
@@ -0,0 +1,83 @@
+﻿using nmct.ssa.dropbox.common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ssa.dropbox.DataAccess
+{
+    public class DAExternalUser
+    {
+        private const string CONNECTIONSTRING = "DefaultConnection";
+
+        private static ExternalUser Create(IDataRecord rec)
+        {
+            return new ExternalUser{
+                Id = Int32.Parse(rec["Id"].ToString()),
+                Blocked = Boolean.Parse(rec["blocked"].ToString()),
+                UserName = rec["UserName"].ToString(),
+                Password = rec["Password"].ToString()
+            };
+        }
+
+        public static List<ExternalUser> GetExternalUsers()
+        {
+            List<ExternalUser> list = new List<ExternalUser>();
+
+            string sql = "SELECT * FROM ExternalUser";
+
+            DbDataReader reader = Database.GetData(CONNECTIONSTRING, sql);
+
+            while (reader.Read())
+                list.Add(Create(reader));
+
+            return list;
+        }
+
+        public static ExternalUser GetById(int id)
+        {
+            string sql = "SELECT * FROM ExternalUser WHERE Id=@Id";
+
+            DbParameter parId = Database.AddParameter(CONNECTIONSTRING, "@Id", id);
+
+            DbDataReader reader = Database.GetData(CONNECTIONSTRING, sql, parId);
+
+            ExternalUser res;
+            if (!reader.Read())
+                res = null;
+            else
+                res = Create(reader);
+
+            reader.Close();
+
+            return res;
+
+
+        }
+
+        public static void Update(ExternalUser ext)
+        {
+            string sql = "UPDATE ExternalUser SET UserName=@UserName,Password=@Password,Blocked=@Blocked WHERE Id=@Id";
+
+            DbParameter parId = Database.AddParameter(CONNECTIONSTRING, "@Id", ext.Id);
+            DbParameter parUser = Database.AddParameter(CONNECTIONSTRING, "@UserName", ext.UserName);
+            DbParameter parPass = Database.AddParameter(CONNECTIONSTRING, "@Password", ext.Password);
+            DbParameter parBlocked = Database.AddParameter(CONNECTIONSTRING, "@Blocked", ext.Blocked);
+
+            Database.ModifyData(CONNECTIONSTRING, sql, parId, parUser, parPass, parBlocked);
+        }
+
+        public static int New(ExternalUser ext)
+        {
+            string sql = "INSERT INTO ExternalUser VALUES(@UserName,@Password,@Blocked)";
+
+            DbParameter parUser = Database.AddParameter(CONNECTIONSTRING, "@UserName", ext.UserName);
+            DbParameter parPass = Database.AddParameter(CONNECTIONSTRING, "@Password", ext.Password);
+            DbParameter parBlocked = Database.AddParameter(CONNECTIONSTRING, "@Blocked", ext.Blocked);
+
+            return Database.InsertData(CONNECTIONSTRING, sql, parUser, parPass, parBlocked);
+        }
+    }
+}
